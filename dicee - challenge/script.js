381d181aac0d9052113cf28d653bd89a58625522
Lóris <47677782+sirloris@users.noreply.github.com>
2021-11-26 13:10:44 -0300

    add dicee-challenge

--- dicee - challenge/script.js
+++ dicee - challenge/script.js
@@ -1,3 +1,17 @@
-var loop = true,
-	alt1 = Math.floor(Math.random() * 6) + 1,
-	alt2 = Math.floor(Math.random() * 6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1,
+	randomNumber2 = Math.floor(Math.random() * 6) + 1,
+	dice1 = "img/" + randomNumber1 + ".png",
+	dice2 = "img/" + randomNumber2 + ".png",
+	img1 = document.getElementById("img1"),
+	img2 = document.getElementById("img2");
+
+img1.src = dice1;
+img2.src = dice2;
+
+if (randomNumber1 > randomNumber2) {
+	document.getElementById("winner").innerHTML = "Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+	document.getElementById("winner").innerHTML = "Player 2 Wins!";
+} else {
+	document.getElementById("winner").innerHTML = "Draw!";
+}
